f9dd624b1938990e3d5045552bc9bd33b42d12a8
Zeeshan Nawaz <35934083+xee95@users.noreply.github.com>
2025-04-22 00:29:10 +0500

    update

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,10 +1,14 @@
 import { LoginForm } from "../../components/login-form";
 
 export default function LoginPage() {
+  const handleToggle = () => {
+    // logic to switch to RegisterPage (or open a modal, etc.)
+    console.log("Switch to Register Page");
+  };
   return (
     <div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
       <div className="w-full max-w-sm md:max-w-3xl">
-        <LoginForm />
+        <LoginForm onToggle={handleToggle} />
       </div>
     </div>
   );
